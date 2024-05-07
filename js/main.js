f241e5a3b2139390767fdc83c035ea4c03e8fc31
Gabriel Munhak <heclypso@gmail.com>
2024-05-07 06:29:06 -0300

    primeiro commit

--- js/main.js
+++ js/main.js
@@ -1,26 +1,20 @@
 
 $('#telefone').mask('(00) 00000-0000')
-$('#cep').mask('00000-000')
-$('#cpf').mask('000.000.000-00')
 
 $('form').validate({
     rules: {
-        cep: {
-            required: true,
-            email: true
-        },
-
-        cpf: {
-            required: true,
-            email: true
-        },
-        
-        telefone: {
+        nome: {
             required: true
         },
+
+        email: {
+            required: true
+        },
+        
     },
     messages: {
-        nome: 'Por favor ensira o seu nome'
+        nome: 'Por favor ensira o seu nome',
+        email: 'Por favor ensira o seu e-mail'
         
     },
     submitHandler: function(form){
